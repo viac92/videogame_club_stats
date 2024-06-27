139b58a8a88805ad4ee61ba8e1ae266e7f631a95
viac92 <64146594+viac92@users.noreply.github.com>
2024-06-27 10:58:34 +0200

    first commit

---
+++ web_app/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>Videogame Club Stats</h1>
+    </div>
+  </header>
+  <main>
+    <div>
+      Stats
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: center;
+  }
+  main {
+    display: flex;
+    justify-content: center;
+  }
+</style>
