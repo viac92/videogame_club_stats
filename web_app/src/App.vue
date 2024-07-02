51fd2e32cd67b9cb6e03ad8648e249aaf427d568
viac92 <64146594+viac92@users.noreply.github.com>
2024-07-02 17:21:09 +0200

    first implementation

--- web_app/src/App.vue
+++ web_app/src/App.vue
@@ -1,26 +1,88 @@
 <script setup>
-</script>
+import { onMounted, onUnmounted, ref } from 'vue';
+import Chart from 'chart.js/auto';
+import {
+  playerTotalPresenceData,
+  playedGamesData, 
+  playersPresenceByDayData, 
+  playedGamesGenresData, 
+} from './data.js';
 
+const playerTotalPresenceConfig = {
+  type: 'bar',
+  data: playerTotalPresenceData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+
+const playedGameConfig = {
+  type: 'doughnut',
+  data: playedGamesData,
+};
+
+const playersPresenceByDayConfig = {
+  type: 'line',
+  data: playersPresenceByDayData,
+};
+
+const playedGamesGenresConfig = {
+  type: 'radar',
+  data: playedGamesGenresData,
+  options: {
+    elements: {
+      line: {
+        borderWidth: 3
+      }
+    }, 
+    scales: {
+      r: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+
+onMounted(() => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  new Chart(ctx, playerTotalPresenceConfig);
+  
+  const ctxDoughnut = document.getElementById('myChartDoughnut').getContext('2d');
+  new Chart(ctxDoughnut, playedGameConfig);
+
+  const ctxLine = document.getElementById('myChartLine').getContext('2d');
+  new Chart(ctxLine, playersPresenceByDayConfig);
+
+  const ctxRadar = document.getElementById('myChartRadar').getContext('2d');
+  new Chart(ctxRadar, playedGamesGenresConfig);
+});
+</script>
 <template>
-  <header>
+  <div class="canvas-container">
     <div>
       <h1>Videogame Club Stats</h1>
     </div>
-  </header>
-  <main>
     <div>
-      Stats
+      <canvas id="myChart"></canvas>
     </div>
-  </main>
+    <div>
+      <canvas id="myChartLine"></canvas>
+    </div>
+    <div>
+      <canvas id="myChartDoughnut"></canvas>
+    </div>
+    <div>
+      <canvas id="myChartRadar"></canvas>
+    </div>
+  </div>
 </template>
-
 <style scoped>
-  header {
-    display: flex;
-    justify-content: center;
-  }
-  main {
-    display: flex;
-    justify-content: center;
-  }
+canvas {
+    width: 100%;
+    object-fit: contain;
+}
 </style>
